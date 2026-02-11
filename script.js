8353a2d6039bbbc279963e66d536fda44ff55a52
unknown <leonelmunter@gmail.com>
2026-02-11 14:55:09 -0300

    contact form

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
     $form.addEventListener("submit", e=>{
         e.preventDefault();
         $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/info.institutoenfocate@gmail.com", {
+        fetch("https://formsubmit.co/ajax/be9d3638ef864a7138192f37bf94b138", {
             method: "POST",
             body: new FormData(e.target),
         }).then((res)=> res.ok ? res.json() : Promise.reject(res))
